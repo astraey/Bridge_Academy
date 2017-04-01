6edb3dce5228e0afbb99b8e4b241669a85168c35
Aitor Cubeles Torres <aitorct7@gmail.com>
2017-04-01 15:14:11 +0100

    Serve dynamic file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import home, singlePrize, exercises, prizes, profile, singleExercise
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return app.send_static_file('index.html')
+
+#@app.route('/hello/<user>')
+#def hello_name(user):
+#   return render_template('hello.html', name = user)
 
 if __name__ == '__main__':
     app.run()
